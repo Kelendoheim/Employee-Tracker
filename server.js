c2a025731f1af51bede50943c37f8643396e4fe6
TDWalsh <thomas.walsh736@gmail.com>
2020-10-06 19:55:14 -0400

    added inquirer structure and basic user flow logic for the add feature

--- server.js
+++ server.js
@@ -18,13 +18,64 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  afterConnection();
+  initPrompt();
 });
 
+
+
+
+
+function initPrompt() {
+    inquirer
+    .prompt([
+      {
+        name: 'action',
+        type: 'list',
+        message: 'Would you like to view, update or add to your employment team?',
+        choices: ["Add", "View", "Update"]
+      }
+    ]).then((answers) =>{
+        console.log(answers.action)
+        if(answers.action === "Add"){
+            addToData();
+        }else if (answers.action === "View"){
+            viewData();
+        }else if (answers.action === "Update"){
+            updateData();
+        }
+        
+    })
+    
+};
+
+
+
 function afterConnection() {
-  connection.query("SELECT * FROM employee", function(err, res) {
-    if (err) throw err;
-    console.table(res);
-    connection.end();
-  });
+    connection.query("SELECT * FROM employee", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        afterAfterConnection();
+    });
 }
+
+function addToData(){
+    inquirer.prompt([
+        {
+        name: 'addTarget',
+        type: 'list',
+        message: 'Would you like to add a department, a role or an employee?',
+        choices: ["Department", "Role", "Employee"]
+        }
+    ]).then((answers) => {
+        console.log(answers.addTarget)
+        if(answers.addTarget === "Department"){
+            addDepartment();
+        }else if (answers.addTarget === "Role"){
+            addRole();
+        }else if (answers.addTarget === "Employee"){
+            addEmployee();
+        }
+        
+    })
+}
+
